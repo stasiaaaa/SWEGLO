45cc21b5f1cbf036b12414c6662aa334841afc55
Stasia <anastasia.saprunova@gmail.com>
2026-02-27 23:03:33 +0100

    Added layers for NATO members.

--- back-end/app.py
+++ back-end/app.py
@@ -18,5 +18,23 @@ def get_relations(country_name):
     else:
         return jsonify({'error': 'No data found'}), 404
 
+
+@app.route('/api/nato', methods=['GET'])
+def get_nato_members():
+    with open('data/nato_members.json') as f:
+        data = json.load(f)
+    return jsonify(data)
+
+@app.route('/api/nato/<country_name>', methods=['GET'])
+def get_is_nato_member(country_name):
+    with open('data/nato_members.json') as f:
+        data = json.load(f)
+    
+    match = next((r for r in data if r["country"].lower() == country_name.lower()), None)
+
+    if match:
+        return jsonify(match)
+    return jsonify({'nato_member': 'No'}) # TODO: countryname from input
+
 if __name__ == '__main__':
     app.run(debug=True)
